808fbfbf2e609d370a458b1a6fd2a7942199c642
Ihsan Fauzan <ihsanfauzan76@gmail.com>
2023-03-12 01:13:42 +0700

    fix kitchen service

--- kitchen-service/index.js
+++ kitchen-service/index.js
@@ -1,26 +1,27 @@
 const express = require('express')
-const app = express()
-
-const port = 0
-const mongoose = require('mongoose')
-
-mongoose.connect(`mongodb://localhost:27017/ets_menu_service`).then(() => {
-    serviceLog(`Successfully connected to database`)
-})
-
 const bodyParser = require('body-parser')
 const { serviceLog } = require('./utils')
 const Menu = require('./schemas/Menu')
 
+const app = express()
+const port = 8111
+const mongoose = require('mongoose')
+
+mongoose.connect(`mongodb://localhost:27017/ets_kitchen_service`).then(() => {
+    serviceLog(`Successfully connected to database`)
+})
+
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
+app.get('/kitchen', async (req, res) => {
     let data = await Menu.find({})
     res.status(200).json(data)
 })
 
-app.post('/', async (req, res) => {
+app.post('/kitchen/menu', async (req, res) => {
     const data = {
+        id_cafe: req.body.id_cafe,
+        id_outlet: req.body.id_outlet,
         name: req.body.name,
         price: req.body.price
     }
@@ -32,19 +33,21 @@ app.post('/', async (req, res) => {
 })
 
 
-app.patch('/:id', (req, res) => {
-    const data = {
+app.put('/kitchen/menu/:id', async (req, res) => {
+    let data = {
         name: req.body.name,
-        address: req.body.address
+        price: req.body.price
     }
 
+    await Menu.updateOne({_id: req.params.id}, data)
     res.status(200).json({
         success: true,
         message: "Menu has been updated"
     })
 })
 
-app.delete('/:id', (req, res) => {
+app.delete('/kitchen/menu/:id', async (req, res) => {
+    await Menu.deleteOne({_id: req.params.id})
     res.status(200).json({
         success: true,
         message: "Menu has been deleted"
